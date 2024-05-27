33d0d26b29e698571fa56b0b3bbcb96615a1a3d1
MinMinAung1 <minaungm845@gmail.com>
2024-05-27 21:21:02 +0630

    react basic testing

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo text='Learn React'/>
-      <Todo text='Learn Spring'/>
-      <Todo text='Learn PHP'/>
-      <Todo text='Learn Java'/>
-      <Todo text='Learn C#'/>
+      <Todo text="Learn React" />
+      <Todo text="Learn Spring" />
+      <Todo text="Learn PHP" />
+      <Todo text="Learn Java" />
+      <Todo text="Learn C#" />
     </div>
   );
 }
